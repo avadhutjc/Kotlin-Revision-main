0a3e2cc83853e0654f2d172b186376dc0d9fb0e8
Avadhutjc <Avadhutjc@gmail.com>
2022-02-03 18:48:05 +0530

    Completed

--- src/JavaGC.java
+++ src/JavaGC.java
@@ -16,3 +16,19 @@ object is garbage collected
 object is garbage collected
 Process finished with exit code 0
 */
+
+//Ex. By creating anonymous object
+//public class JavaGC {
+//    public void finalize() {
+//        System.out.println("object is garbage collected");
+//    }
+//
+//    public static void main(String[] args) {
+//        new JavaGC(); //creating anonymous obj
+//        System.gc();
+//    }
+//}
+///*
+//object is garbage collected
+//Process finished with exit code 0
+//*/
